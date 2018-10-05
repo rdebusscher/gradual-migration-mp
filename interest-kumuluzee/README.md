ea858cf7b3a10ed472effa676c7d3e3e73574eea
Rudy De Busscher <rdebusscher@gmail.com>
2018-10-05 11:44:01 +0200

    demo 2 - interest service code

---
+++ interest-kumuluzee/README.md
@@ -0,0 +1,24 @@
+# MicroService for Interest domain
+
+Implementation with KumuluzEE
+
+## interest-service-api artifact
+
+Reusable artifact for 'server' and 'client'.
+
+ 
+## interest-service
+
+MicroProfile application using KumuluzEE exposing the interest endpoint
+
+## Run
+
+mvn clean install
+
+./start.sh
+
+## Test
+
+http://localhost:9999/data/interest/10000?years=15
+
+
---
+++ interest-kumuluzee/interest-service-api/src/main/java/be/atbash/mp/gradual/mortgage/kumuluzee/interest/api/CalculatedInterest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Rudy De Busscher (https://www.atbash.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.atbash.mp.gradual.mortgage.kumuluzee.interest.api;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CalculatedInterest {
+
+    private int months;
+    private double value;
+    private double rate;
+
+    public CalculatedInterest() {
+    }
+
+    public CalculatedInterest(int months, double value, double rate) {
+        this.months = months;
+        this.value = value;
+        this.rate = rate;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+}
